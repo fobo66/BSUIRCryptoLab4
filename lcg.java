d71f237607893c456145e532e1de2f93a0cf0072
irisyuan <irisyuan@nyu.edu>
2014-08-09 23:54:53 -0400

    Fixed weird spacing, comments, and Scanner

--- lcg.java
+++ lcg.java
@@ -1,5 +1,5 @@
 /* 	
-LCG implementation
+LCG implementation for learning purposes based on http://en.wikipedia.org/wiki/Linear_congruential_generator
 Iris Yuan
 3/6/2014
 */
@@ -10,8 +10,6 @@ import java.lang.Math;
 
 public class lcg {
 
-	 public static Scanner scanner = new Scanner(System.in);
-	 
 	 // basic recursive GCD method for use when given x0,x1... compute m
 	 public static int gcd(int number1, int number2) {
 	        //base case
@@ -44,24 +42,26 @@ public class lcg {
 		 int[] array = new int[n+1];
 		 System.out.print("x values: ");
 		 for (int i = 0; i < n+1; i ++) {
-			 x = (a * x + b) % m;
+			 x = (a * x + b) % m; // LCG formula
 			 System.out.println(x);
 			 array[i] = x;
 		 }
-		 
 		 return array;
 	 }
 	 
-	 public static void main (String[] args)
-     {
-    	 // input positive integers l and n 
-         System.out.print( "Input positive integer l: " );
-         int l = scanner.nextInt();
-         System.out.print( "Input positive integer n: " );
-         int n = scanner.nextInt();
-         
-         // given a, b, and m, compute x0, x1, x2... 
-         int[] array = computeX(l,n);
+	 // main method calls computeX with user inputs as arguments
+	 public static void main (String[] args) {
+	 	
+	 	 Scanner scanner = new Scanner(System.in);
 
-     } 
-}
+	    	 // input positive integers l and n 
+	         System.out.print( "Input positive integer l: " );
+	         int l = scanner.nextInt();
+	         System.out.print( "Input positive integer n: " );
+	         int n = scanner.nextInt();
+	         
+	         // given a, b, and m, compute x0, x1, x2... 
+	         int[] array = computeX(l,n);
+     	} 
+     	
+} // end class lcg
