ea6db9ce763f29e1251e949a239d37537d6f2f0c
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-04 21:18:12 +0300

    Setup annotations for easier conversion

--- src/main/java/dev/fobo66/crypto/DES.java
+++ src/main/java/dev/fobo66/crypto/DES.java
@@ -1,39 +1,41 @@
 package dev.fobo66.crypto;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Arrays;
 
 public class DES {
 
     // initialization vector for CBC, CFB and OFB modes
-    private static byte[] IV = {(byte) 220, (byte) 190, 106, (byte) 231, (byte) 234, 93, 92, 97};
+    private static final byte[] IV = {(byte) 220, (byte) 190, 106, (byte) 231, (byte) 234, 93, 92, 97};
 
     // initial permutation table
-    private static int[] IP = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22,
+    private static final int[] IP = {58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22,
             14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53,
             45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
     // inverse initial permutation
-    private static int[] invIP = {40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22,
+    private static final int[] invIP = {40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22,
             62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2,
             42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25};
     // Permutation P (in f(Feistel) function)
-    private static int[] P = {16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9,
+    private static final int[] P = {16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9,
             19, 13, 30, 6, 22, 11, 4, 25
 
     };
     // initial key permutation 64 => 56 bit
-    private static int[] PC1 = {57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19,
+    private static final int[] PC1 = {57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19,
             11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21,
             13, 5, 28, 20, 12, 4};
     // key permutation at round i 56 => 48
-    private static int[] PC2 = {14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
+    private static final int[] PC2 = {14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
             41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
     // key shift for each round
-    private static int[] keyShift = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+    private static final int[] keyShift = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
     // expansion permutation from function f
-    private static int[] expandTbl = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16,
+    private static final int[] expandTbl = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16,
             17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1};
     // substitution boxes
-    private static int[][][] sboxes = {
+    private static final int[][][] sboxes = {
             {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
                     {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
                     {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
@@ -73,7 +75,7 @@ public class DES {
 
             }};
 
-    private static void setBit(byte[] data, int pos, int val) {
+    private static void setBit(byte @NotNull [] data, int pos, int val) {
         int posByte = pos / 8;
         int posBit = pos % 8;
         byte tmpB = data[posByte];
@@ -82,7 +84,7 @@ public class DES {
         data[posByte] = newByte;
     }
 
-    private static int extractBit(byte[] data, int pos) {
+    private static int extractBit(byte @NotNull [] data, int pos) {
         int posByte = pos / 8;
         int posBit = pos % 8;
         byte tmpB = data[posByte];
@@ -90,7 +92,7 @@ public class DES {
         return bit;
     }
 
-    private static byte[] rotateLeft(byte[] input, int len, int pas) {
+    private static byte @NotNull [] rotateLeft(byte[] input, int len, int pas) {
         int nrBytes = (len - 1) / 8 + 1;
         byte[] out = new byte[nrBytes];
         for (int i = 0; i < len; i++) {
@@ -100,7 +102,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] extractBits(byte[] input, int pos, int n) {
+    private static byte @NotNull [] extractBits(byte[] input, int pos, int n) {
         int numOfBytes = (n - 1) / 8 + 1;
         byte[] out = new byte[numOfBytes];
         for (int i = 0; i < n; i++) {
@@ -111,7 +113,7 @@ public class DES {
 
     }
 
-    private static byte[] permute(byte[] input, int[] table) {
+    private static byte @NotNull [] permute(byte @NotNull [] input, int @NotNull [] table) {
         int nrBytes = (table.length - 1) / 8 + 1;
         byte[] out = new byte[nrBytes];
         for (int i = 0; i < table.length; i++) {
@@ -122,7 +124,7 @@ public class DES {
 
     }
 
-    private static byte[] xorBytes(byte[] a, byte[] b) {
+    private static byte @NotNull [] xorBytes(byte @NotNull [] a, byte @NotNull [] b) {
         byte[] out = new byte[a.length];
         for (int i = 0; i < a.length; i++) {
             out[i] = (byte) (a[i] ^ b[i]);
@@ -131,7 +133,7 @@ public class DES {
 
     }
 
-    private static byte[] encrypt64Block(byte[] block, byte[] key, boolean isDecrypt) {
+    private static byte @NotNull [] encrypt64Block(byte @NotNull [] block, byte @NotNull [] key, boolean isDecrypt) {
         byte[] result = new byte[block.length];
         byte[] R = new byte[block.length / 2];
         byte[] L = new byte[block.length / 2];
@@ -158,7 +160,7 @@ public class DES {
         return result;
     }
 
-    private static byte[] f_func(byte[] R, byte[] K) {
+    private static byte @NotNull [] f_func(byte @NotNull [] R, byte @NotNull [] K) {
         byte[] result;
         result = permute(R, expandTbl);
         result = xorBytes(result, K);
@@ -167,7 +169,7 @@ public class DES {
         return result;
     }
 
-    private static byte[] s_func(byte[] in) {
+    private static byte @NotNull [] s_func(byte @NotNull [] in) {
         in = separateBytes(in, 6);
         byte[] out = new byte[in.length / 2];
         int halfByte = 0;
@@ -184,7 +186,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] separateBytes(byte[] in, int length) {
+    private static byte @NotNull [] separateBytes(byte @NotNull [] in, int length) {
         int numOfBytes = (8 * in.length - 1) / length + 1;
         byte[] out = new byte[numOfBytes];
         for (int i = 0; i < numOfBytes; i++) {
@@ -196,7 +198,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] concatBits(byte[] a, int aLen, byte[] b, int bLen) {
+    private static byte @NotNull [] concatBits(byte @NotNull [] a, int aLen, byte @NotNull [] b, int bLen) {
         int numOfBytes = (aLen + bLen - 1) / 8 + 1;
         byte[] out = new byte[numOfBytes];
         int j = 0;
@@ -213,7 +215,7 @@ public class DES {
         return out;
     }
 
-    private static byte[] deletePadding(byte[] input) {
+    private static byte @NotNull [] deletePadding(byte[] input) {
         int count = 0;
 
         int i = input.length - 1;
@@ -227,7 +229,7 @@ public class DES {
         return result;
     }
 
-    private static byte[][] generateSubKeys(byte[] key) {
+    private static byte[] @NotNull [] generateSubKeys(byte @NotNull [] key) {
         byte[][] result = new byte[16][];
         byte[] tmpK = permute(key, PC1);
 
@@ -247,7 +249,7 @@ public class DES {
         return result;
     }
 
-    public static byte[] encrypt(byte[] data, byte[] key, DESMode mode) {
+    public static byte @NotNull [] encrypt(byte @NotNull [] data, byte @NotNull [] key, DESMode mode) {
         int i;
         int length = 8 - data.length % 8;
         byte[] padding = new byte[length];
@@ -302,52 +304,6 @@ public class DES {
             block = encrypt64Block(block, key, false);
             System.arraycopy(block, 0, result, i - 8, block.length);
         }
-        return result;
-    }
-
-    public static byte[] decrypt(byte[] data, byte[] key, DESMode mode) {
-        int i;
-        byte[] result = new byte[data.length];
-        byte[] block = new byte[8];
-        byte[] processedBlock = new byte[8];
-        byte[] feedback = new byte[8];
-        System.arraycopy(IV, 0, feedback, 0, feedback.length);
-
-        for (i = 0; i < data.length; i++) {
-            if (i > 0 && i % 8 == 0) {
-                switch (mode) {
-
-                    case ECB:
-                        processedBlock = encrypt64Block(block, key, true);
-                        break;
-                    case CBC:
-                        processedBlock = encrypt64Block(block, key, true);
-                        processedBlock = xorBytes(feedback, processedBlock);
-                        feedback = Arrays.copyOfRange(block, 0, 8);
-                        break;
-                    case CFB:
-                        processedBlock = encrypt64Block(key, feedback, false);
-                        feedback = Arrays.copyOfRange(processedBlock, 0, 8);
-                        processedBlock = xorBytes(processedBlock, block);
-                        break;
-                    case OFB:
-                        processedBlock = encrypt64Block(feedback, key, false);
-                        feedback = Arrays.copyOfRange(processedBlock, 0, 8);
-                        processedBlock = xorBytes(processedBlock, block);
-                        break;
-                    case CTR:
-                        throw new IllegalArgumentException("We don't decrypt in CTR mode");
-                }
-                System.arraycopy(processedBlock, 0, result, i - 8, block.length);
-            }
-            if (i < data.length)
-                block[i % 8] = data[i];
-        }
-        block = encrypt64Block(block, key, true);
-        System.arraycopy(block, 0, result, i - 8, block.length);
-
-        result = deletePadding(result);
-
         return result;
     }
 }
