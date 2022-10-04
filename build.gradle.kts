553fee2c15dc09bcd157bb905d10c152036d1a47
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2022-10-04 21:11:51 +0300

    Changed JVM target

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     application
     kotlin("jvm") version "1.7.20"
@@ -11,12 +13,16 @@ allprojects {
     repositories {
         mavenCentral()
     }
+}
 
-    dependencies {
-        implementation("org.apache.commons:commons-rng-core:1.4")
-        implementation("org.apache.commons:commons-rng-simple:1.4")
-        implementation("org.apache.commons:commons-rng-client-api:1.4")
-        implementation("org.jetbrains:annotations:23.0.0")
-        implementation("commons-cli:commons-cli:1.5.0")
-    }
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "11"
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-rng-core:1.4")
+    implementation("org.apache.commons:commons-rng-simple:1.4")
+    implementation("org.apache.commons:commons-rng-client-api:1.4")
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("commons-cli:commons-cli:1.5.0")
 }
